794a9f99d3cf0143ab0a97dec50ed73c150da92f
Fikrym2021 <93166452+Fikrym2021@users.noreply.github.com>
2021-10-26 21:56:42 +0700

    Update Profile Post

--- lib/model/profile_post.dart
+++ lib/model/profile_post.dart
@@ -7,34 +7,50 @@ class ProfilePost {
   static List<ProfilePost> postsDummy = [
     ProfilePost(
       imageUrls: [
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%201.png',
+      ],
+    ),
+    ProfilePost(
+      imageUrls: [
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%2016.png',
+      ],
+    ),
+    ProfilePost(
+      imageUrls: [
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%2017.png',
         'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png',
       ],
     ),
     ProfilePost(
       imageUrls: [
-        'https://pyxis.nymag.com/v1/imgs/bf2/b1e/6f6018c7b262f744bf717f22e291fb5b50-alfred-lede.rsquare.w700.jpg',
-        'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png',
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%204.png',
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%204.png',
       ],
     ),
     ProfilePost(
       imageUrls: [
-        'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png',
-        'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png',
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%205.png'
       ],
     ),
     ProfilePost(
       imageUrls: [
-        'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png'
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%206.png',
       ],
     ),
     ProfilePost(
       imageUrls: [
-        'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png',
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%2010.png',
       ],
     ),
     ProfilePost(
       imageUrls: [
-        'https://static.wikia.nocookie.net/gotham-inc/images/b/b1/The_Dark_Knight.png',
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%2011.png',
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%2011.png',
+      ],
+    ),
+    ProfilePost(
+      imageUrls: [
+        'https://raw.githubusercontent.com/Fikrym2021/desain_figma/main/assets/images/Post%2012.png',
       ],
     ),
   ];
